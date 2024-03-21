b54f2cbfd9022dc8512f777f73620f9cad30f91e
TMTCo <149132955+TMTCo@users.noreply.github.com>
2024-03-21 13:22:50 +1100

    Update everythingserver-2-setup.sh

--- everythingserver-2-setup.sh
+++ everythingserver-2-setup.sh
@@ -2,46 +2,46 @@
 
 # Update apt repository
 echo "Updating apt repository..."
-sudo apt update
+sudo apt update -qq
 
 # Upgrade packages
 echo "Upgrading packages..."
-sudo apt upgrade -y
+sudo apt upgrade -y -qq
 
 # Install wget (required for downloading Webmin GPG key)
 echo "Installing wget..."
-sudo apt install wget -y
+sudo apt install wget -y -qq
 
 # Install Webmin
 echo "Installing Webmin..."
 sudo sh -c 'echo "deb https://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
 wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
 sudo apt update
-sudo apt install webmin -y
+sudo apt install webmin -y -qq
 
 # Install Samba
 echo "Installing Samba..."
-sudo apt install samba -y
+sudo apt install samba -y -qq
 
 # Install Apache2
 echo "Installing Apache2..."
-sudo apt install apache2 -y
+sudo apt install apache2 -y -qq
 
 # Install Squid Proxy
 echo "Installing Squid Proxy..."
-sudo apt install squid -y
+sudo apt install squid -y -qq
 
 # Install Pi-hole
 echo "Installing Pi-hole..."
 git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
 cd "Pi-hole/automated install/"
-sudo bash basic-install.sh
+sudo bash basic-install.sh -qq
 
 # Install Docker
 echo "Installing Docker..."
-sudo apt install docker.io -y
+sudo apt install docker.io -y -qq
 
 # Restart Samba service
-sudo systemctl restart smbd
+sudo systemctl restart smbd 
 
 echo "Setup completed."
